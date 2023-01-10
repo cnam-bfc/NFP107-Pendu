4f8179c009c87b3b249b548a6c34f971878642f1
FarmVivi <farmvividev@gmail.com>
2023-01-10 14:58:13 +0100

    Database errors

--- includes/init.php
+++ includes/init.php
@@ -6,7 +6,8 @@ session_start();
 <?php
 // Connexion à la base de données
 try {
-    $mysqlClient = new PDO('mysql:host=10.254.0.10;dbname=cnam_nfp107_pendu;charset=utf8', 'cnam_nfp107_pendu', 'EZ3M38F9W)BY6]BF');
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=cnam_nfp107_pendu;charset=utf8', 'cnam_nfp107_pendu', 'EZ3M38F9W)BY6]BF');
+    $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     $_SESSION['ERROR_MSG'] = 'Erreur de connexion à la base de données:</br>' . $e->getMessage();
     include_once('includes/error.php');
