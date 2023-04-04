330de143bd85a76920e6d1895ac0b383c1fd1c23
FarmVivi <farmvividev@gmail.com>
2023-04-04 23:58:13 +0200

    Database configuration rework via environement variables

--- includes/init.php
+++ includes/init.php
@@ -2,13 +2,80 @@
 // Code obligatoire, doit s'exécuter avant tout code html ou php
 session_start();
 
-define('DATABASE_TYPE', 'firebird');
+// Récupération de la configuration
+// Type de base de données
+if (getenv('DATABASE_TYPE') !== false) {
+    $database_type = getenv('DATABASE_TYPE');
+} else {
+    $database_type = 'mysql';
+}
+
+// Hôte de la base de données
+if (getenv('DATABASE_HOST') !== false) {
+    $database_host = getenv('DATABASE_HOST');
+} else {
+    $database_host = 'localhost';
+}
+
+// Port de la base de données
+if (getenv('DATABASE_PORT') !== false) {
+    $database_port = getenv('DATABASE_PORT');
+} else {
+    $database_port = '3306';
+}
+
+// Nom de la base de données
+if (getenv('DATABASE_NAME') !== false) {
+    $database_name = getenv('DATABASE_NAME');
+} else {
+    $database_name = 'cnam_nfp107_pendu';
+}
+
+// Charset de la base de données
+if (getenv('DATABASE_CHARSET') !== false) {
+    $database_charset = getenv('DATABASE_CHARSET');
+} else {
+    $database_charset = 'utf8';
+}
+
+// Utilisateur de la base de données
+if (getenv('DATABASE_USERNAME') !== false) {
+    $database_username = getenv('DATABASE_USERNAME');
+} else {
+    $database_username = 'cnam_nfp107_pendu';
+}
+
+// Mot de passe de la base de données
+if (getenv('DATABASE_PASSWORD') !== false) {
+    $database_password = getenv('DATABASE_PASSWORD');
+} else {
+    $database_password = '';
+}
+
+// FIREBIRD
+// Rôle de l'utilisateur de la base de données
+if (getenv('DATABASE_FIREBIRD_ROLE') !== false) {
+    $database_role = getenv('DATABASE_FIREBIRD_ROLE');
+}
+
+// Configuration manuelle
+/*
+$database_type = 'firebird';
+$database_host = 'localhost';
+$database_port = '3050';
+$database_name = 'C:\wamp64\www\Projet\SGBD\PENDU.FDB';
+$database_charset = 'utf8';
+$database_username = 'SYSDBA';
+$database_password = 'masterkey';
+// OPTIONNEL $database_role = '';
+*/
 
 // Connexion à la base de données
-switch (DATABASE_TYPE) {
+switch ($database_type) {
     case 'mysql':
         try {
-            $pdo = new PDO('mysql:host=localhost;dbname=cnam_nfp107_pendu;charset=utf8', 'cnam_nfp107_pendu', 'EZ3M38F9W)BY6]BF');
+            $DSN = 'mysql:host=' . $database_host . ';port=' . $database_port . ';dbname=' . $database_name . ';charset=' . $database_charset;
+            $pdo = new PDO($DSN, $database_username, $database_password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (Exception $e) {
             $_SESSION['ERROR_MSG'] = 'Erreur de connexion à la base de données:</br>' . $e->getMessage();
@@ -17,23 +84,14 @@ switch (DATABASE_TYPE) {
         break;
 
     case 'firebird':
-        $host = "localhost";
-        // $database = "C:\wamp64\www\Projet\SGBD\PENDU.FDB";
-        $database = "pendu";
-        // $user = "SYSDBA";
-        $user = "PENDU";
-        // $password = "masterkey";
-        $password = "pendu";
-        $role = "PENDUROLE";
-
         try {
-            $DSN = "firebird:dbname=$host:$database";
-            if (!empty($role)) {
-                $DSN .= ";role=$role";
+            $DSN = 'firebird:dbname=' . $database_host . '/' . $database_port . ':' . $database_name;
+            if (!empty($database_role)) {
+                $DSN .= ';role=' . $database_role;
             }
-            $pdo = new PDO($DSN, $user, $password);
+            $pdo = new PDO($DSN, $database_username, $database_password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // Set column name lowercase
+            // Force column name to be lowercase
             $pdo->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
         } catch (PDOException $e) {
             $_SESSION['ERROR_MSG'] = 'Erreur de connexion à la base de données:</br>' . $e->getMessage();
